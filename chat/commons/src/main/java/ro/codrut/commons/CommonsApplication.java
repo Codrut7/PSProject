1e70d2a02fbdada5afa340152e34f700833a6f80
Codrut7 <43279344+Codrut7@users.noreply.github.com>
2019-03-14 04:13:16 -0700

    Add files via upload

---
+++ chat/commons/src/main/java/ro/codrut/commons/CommonsApplication.java
@@ -0,0 +1,15 @@
+package ro.codrut.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsApplication.class, args);
+	}
+
+}
